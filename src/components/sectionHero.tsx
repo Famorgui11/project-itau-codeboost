68ae032380ff625384400330ceeca91357e10a3d
Fabiano Guimarães <guimaraes_fabiano11@hotmail.com>
2023-09-13 22:39:58 -0300

    feat: start of creation of the hero section component

---
+++ src/components/sectionHero.tsx
@@ -0,0 +1,9 @@
+const SectionHero = () => {
+	return (
+		<section className="w-full h-[44rem] bg-img_bg_hero bg-no-repeat bg-center bg-cover">
+			<h1>Itaú CodeBOOST</h1>
+		</section>
+	);
+};
+
+export default SectionHero;
